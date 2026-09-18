8d8ee26e19b5defaf6474a1c3fb5bc09a92593ba
ANJALI RAGHAV <anjaliraghav190@gmail.com~>
2026-09-18 14:51:54 +0530

    modifing lab1

--- lab1/DOM3.js
+++ lab1/DOM3.js
@@ -5,6 +5,7 @@ const button=new EventEmitter();
 button.on("click",(uname)=>{
     console.log(`button clicked by ${uname}`);
 });
+// task 1 and task 2 will be executed when the button is clicked
 
 button.emit("click", "Raju");
 button.emit("click", "Ramesh");
